981bed0877daf9f646a42cce74efef5cc5fdd39c
Jose Ugal <jugal@codefastly.net>
2021-09-01 23:13:39 +0200

    Commit test

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+
+// ** Navegación
+
+import { HomeScreen } from './src/screens'
 
 export default function App () {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Buenos días</Text>
+      <HomeScreen />
     </View>
   )
 }
